f26dd8e0f7c57aba577d66978b9c34216627196a
alan <alan.wiedmer@ceff.ch>
2023-08-22 07:49:23 +0200

    exercice2

---
+++ exercice_2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+	<title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	</head>
+	<body>
+    <?php
+    echo "\n"."<table border=\"2\">";
+    echo   "\n\t"."<tr>";
+    echo          "\n\t\t"."<td>Nom</td>";
+    echo          "\n\t\t"."<td>Prénom</td>";
+    echo   "\n\t"."</tr>";
+    echo   "\n\t"."<tr>";
+    echo          "\n\t\t"."<td>Lerdorf</td>";
+    echo          "\n\t\t"."<td>Rasmus</td>";
+    echo   "\n\t"."</tr>";
+    echo   "\n\t"."<tr>";
+    echo          "\n\t\t"."<td>Computer</td>";
+    echo          "\n\t\t"."<td>Jules</td>";
+    echo   "\n\t"."</tr>";
+	echo "\n"."</table>";
+?>
+	</body>
+</html>
+
