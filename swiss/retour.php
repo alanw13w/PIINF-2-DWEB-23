d3a02acaf6dd3ec25b2f0dc2bd4b277f7fe99503
alan <alan.wiedmer@ceff.ch>
2023-09-05 16:24:45 +0200

    jeudi 05.09.2023

--- swiss/retour.php
+++ swiss/retour.php
@@ -37,8 +37,7 @@ session_start();
 <body>
 <?php
 echo "<img src='logo-swiss.png' alt='logo swiss air' class='center'>";
-echo "<h2>Réservez vol aller</h2>";
-//echo "<h2>Réservez votre vol</h2>";
+echo "<h2>Réservez vol retour</h2>";
 
 
 //invocations du tableau aeroports et catégories
@@ -48,23 +47,26 @@ require_once ("category.inc.php");
 
 echo "<form action='swissresult.php' method ='post'>";
 
-//création d'une constante par défaut (Genève)Cours inter-entreprise
-define("pardefaut", "GVA");
 
-// importation du tableau en haut dans le champ select
+// importation du tableau en haut dans le champ select départ
 echo "De"."<select name='decolage'>";
 foreach($aeroports AS $key => $dest){
     echo "<option value=\"".$key."\">".$dest."</option>";
 }
 
-//faire une autoselection de notre constante Genève
-echo "<option selected>".pardefaut."</option>";
+//faire une autoselection de notre constante de l'ancienne arrivée
+echo "<option selected>".($aeroports[$_SESSION['vols'][0]['decolage']])."</option>";
 echo "</select>";
 
+// importation du tableau en haut dans le champ select arrivé
 echo "À"."<select class='À' name='arrivé'>";
 foreach($aeroports AS $key => $dest){
     echo "<option value=\"".$key."\">".$dest."</option>";
 }
+
+//faire une autoselection de notre constante de l'ancien départ
+echo "<option selected>".($aeroports[$_SESSION['vols'][0]['arrivé']])."</option>";
+echo "</select>";
 echo "</select>"."<br>";
 
 
@@ -74,7 +76,7 @@ echo "Vol aller"."<input type='date' name='date_depart' value=$date><br><br>";
 //echo "Vol retour"."<input type='text' name='date_retour' value=$demain>"."<br>"."<br>";
 
 
-echo $category['adultes']."<input type='number' name='Adultes' value='1' min='1' max='4'>"."<br>";
+echo $category['adultes']."<input type='number' name='Adultes' value=".$_SESSION['vols'][0]['Adultes']." min='1' max='4'>"."<br>";
 echo $category['enfants']."<input type='number' name='Enfants' value='0' min='0' max='4'>"."<br>";
 echo $category['bebes']."<input class='bebe' type='number' name='Bébés' value='0' min='0' max='4'>"."<br><br>";
 
--- swiss/swissresult.php
+++ swiss/swissresult.php
@@ -10,14 +10,24 @@
 <?php
 //invocations du tableau aeroports
 require_once("aeroports.inc.php");
+require_once ("category.inc.php");
 
 
 
 echo "<h3>Informations concernant votre vol au départ de ".$aeroports[$_POST['decolage']]." à ".$aeroports[$_POST['arrivé']]."</h3>";
+$_SESSION['vols'][] = $_POST;
+
+echo "Départ : ".$aeroports[$_SESSION['vols'][0]['decolage']]." (".$_SESSION['vols'][0]['decolage'].")"."<br>";
+echo "Arrivé : ".$aeroports[$_SESSION['vols'][0]['arrivé']]." (".$_SESSION['vols'][0]['arrivé'].")"."<br>";
+echo "Date de départ : ".$_SESSION['vols'][0]['date_depart']."<br>";
+echo "Passagers : ".$_SESSION['vols'][0]['Adultes']." Adulte(s), ".$_SESSION['vols'][0]['Enfants']." Enfant(s), ".$_SESSION['vols'][0]['Bébés']." Bébé(s)"."<br>";
+
 
 echo "<pre>";
-print_r($_POST);
+print_r($_SESSION);
 echo "</pre>";
+echo "<br>";
+echo "<a href='retour.php'>Réserver un vol retour</a>";
 ?>
 </body>
 </html>
