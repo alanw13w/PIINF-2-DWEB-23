7ce1a00be439503c5e25e8033658d28ad538e178
alan <alan.wiedmer@ceff.ch>
2023-09-26 15:56:31 +0200

    Mardi 26.09.2023

---
+++ Librairies/inscription.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Test - Bootstrap</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="header">
+                    <h3>Inscription</h3>
+                </div>
+            </div>
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    Inscription au portail de jeux
+                </div>
+
+
+                <div class="panel-body">
+                                            <form id="inscription_form">
+                                                <!-- Nom -->
+                                                <div class="form-group row">
+                                                    <label for="nom_per" class="col-sm-2 col-form-label">Nom</label>
+                                                    <div class="col-sm-10">
+                                                        <input type="text" class="form-control" id="nom_per" name="nom_per" placeholder="votre nom">
+                                                    </div>
+                                                </div>
+                                                <!-- Prenom -->
+                                                <div class="form-group row">
+                                                    <label for="prenom_per" class="col-sm-2 col-form-label">Prenom</label>
+                                                    <div class="col-sm-10">
+                                                        <input type="text" class="form-control" id="prenom_per" name="nom_per" placeholder="votre prenom">
+                                                    </div>
+                                                </div>
+                                                <!-- E-mail -->
+                                                <div class="form-group row">
+                                                    <label for="email_per" class="col-sm-2 col-form-label">E-mail</label>
+                                                    <div class="col-sm-10">
+                                                        <input type="text" class="form-control" id="email_per" name="email_per" placeholder="votre adrresse e-mail">
+                                                    </div>
+                                                </div>
+                                                <!-- mdp -->
+                                                <div class="form-group row">
+                                                    <label for="password" class="col-sm-2 col-form-label">Mot de passe</label>
+                                                    <div class="col-sm-10">
+                                                        <input type="text" class="form-control" id="password" name="password" placeholder="votre mot de passe">
+                                                    </div>
+                                                </div>
+                                                <!-- vérification du mdp -->
+                                                <div class="form-group row">
+                                                    <label for="password_conf" class="col-sm-2 col-form-label">Mot de passe(confirmation)</label>
+                                                    <div class="col-sm-10">
+                                                        <input type="text" class="form-control" id="password_conf" name="password_conf" placeholder="Saissisez votre mot de passe une seconde fois">
+                                                    </div>
+                                                </div>
+                                                <!-- Check Box -->
+                                                <div class="form-group row">
+                                                    <div class="col-sm-offset-2 checkbox">
+                                                        <label class="col-sm-10 col-form-label">
+                                                            <input type="checkbox" id="news_letter" name="news_letter">La formation d'informaticien m'intéresse
+                                                        </label>
+                                                    </div>
+                                                </div>
+                                                <!-- Bouttons de la validation validé ou annulation annulé-->
+                                                <div class="form-group row">
+                                                    <div class="col-sm-2 col-sm-offset-8">
+                                                        <input type="submit" class="form-control input-sm btn-primary" id="submit_conf" name="submit_conf" value="S'inscrire">
+                                                    </div>
+                                                    <div class="col-sm-2">
+                                                        <input type="reset" class="form-control input-sm btn-warning" id="reset_con" name="reset_con" value="Annuler">
+                                                    </div>
+                                                </div>
+                                            </form>
+                </div>
+                <div class="panel-footer">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
--- jQuery/morpion/morpion.js
+++ jQuery/morpion/morpion.js
@@ -1,24 +1,121 @@
 $(function (){
+    //ajustements css
+    $('td')/*.css('background-color', 'grey')*/.css('height', "256px").css("width", "256px");
+    $('table').css('margin', 'auto');
 
-    var timeo = 0;
+    //lorsqu'on clique sur voiture ou crois/rond, l'icone change dans les case
+    $('.bouton1').click(function(){
+        $('.symbole_0').css('background-image', 'url("./image/00.png")');
+        $('.symbole_1').css('background-image', 'url("./image/01.png")');
+    });
+    $('.bouton2').click(function(){
+        $('.symbole_0').css('background-image', 'url("./image/porsche.png")');
+        $('.symbole_1').css('background-image', 'url("./image/lambo.png")');
+    });
 
 
-    if(timeo === 0){
-        $('td').click(function(){
-            $(this).addClass("croix");
-            $(this).addClass("symbole_1");
-            timeo ++;
-        });
+    //initialisation des variables
+    var turn = 1;
+    var player = 0;
+    var winner;
+    var $cell;
+    var map = ['2','2','2','2','2','2','2','2','2',];
+    var points = [0,0]
+
+
+
+    //affichage des croix et des rond un à la fois lorsqu'on clique
+    $('td').on('click', function (){
+        if($(this).hasClass('symbole_0') == false && $(this).hasClass('symbole_1') == false){
+
+            $(this).addClass('symbole_' +player);
+
+            Calcul();
+
+            if (player == 0){
+                player = 1;
+            }
+            else{
+                player = 0;
+            }
+
+            turn++;
+            UiPlayerTurn();
+        }
+
+    });
+
+    //fonction calcul qui permet de placer notre croix/rond dans le tableau
+    function Calcul(){
+
+        for (let i = 0; i < 9; i++) {
+
+            $cell = $('td:eq('+i+')');
+
+            if($cell.hasClass('symbole_0')){
+                map[i] = 0;
+            }
+            else if($cell.hasClass('symbole_1')){
+                map[i] = 1;
+            }
+            else{
+                map[i] = 2;
+            }
+
+        }
+
+        //détection du gagnant avec les élément de test
+        if(map[0] == player && map[1] == player && map[2]==player){
+            Victoire();
+        }
+        else if(map[3] == player && map[4] == player && map[5]==player){
+            Victoire();
+        }
+        else if(map[6] == player && map[7] == player && map[8]==player){
+            Victoire();
+        }
+        else if(map[0] == player && map[3] == player && map[6]==player){
+            Victoire();
+        }
+        else if(map[1] == player && map[4] == player && map[7]==player){
+            Victoire();
+        }
+        else if(map[2] == player && map[5] == player && map[8]==player){
+            Victoire();
+        }
+        else if(map[0] == player && map[4] == player && map[8]==player){
+            Victoire();
+        }
+        else if(map[2] == player && map[4] == player && map[6]==player){
+            Victoire();
+        }
+        else if (turn >= 9){
+            Egalité();
+        }
+
 
     }
 
-    else if(timeo === 1){
-        $('td').click(function() {
-            $(this).addClass("rond");
-            $(this).addClass("symbole_0");
-        });
-        timeo = 0;
+    //affichage du vainqueur
+    function Victoire(){
+        winner = player + 1;
+        alert('le joueur '+winner+' a gagné');
+        Reset();
     }
+
+    //affichage de si il y a une égalité
+    function Egalité(){
+        alert('Egalité');
+        Reset();
+    }
+
+    //remise à 0
+    function Reset(){
+        $('td').removeClass('symbole_0').removeClass('symbole_1');
+        map.length = 0;
+        turn = 0;
+    }
+
 
 
 });
